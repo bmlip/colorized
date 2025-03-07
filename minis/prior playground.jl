cc8bc1b4a5d5da0921b963cf1d070e9bc8ac60a1
Fons van der Plas <fonsvdplas@gmail.com>
2025-03-07 17:04:03 +0100

    Update prior playground.jl

--- minis/prior playground.jl
+++ minis/prior playground.jl
@@ -17,7 +17,7 @@ macro bind(def, element)
 end
 
 # ╔═╡ 85b25128-dca3-11ef-1395-5181ae1f5704
-using Plots, Distributionse
+using Plots, Distributions
 
 # ╔═╡ ac34637d-7b89-4938-a262-c35adda8ba06
 using PlutoUI
