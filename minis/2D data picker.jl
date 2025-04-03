b5887375adc788866845a55cf959a297220a9cdf
Fons van der Plas <fonsvdplas@gmail.com>
2025-04-03 11:30:22 +0200

    cleanup

--- minis/2D data picker.jl
+++ minis/2D data picker.jl
@@ -28,6 +28,9 @@ init = [
 	for _ in 1:100
 ]
 
+# ╔═╡ 20981265-4bec-49e8-a0a3-1e1179d2f94f
+
+
 # ╔═╡ ef95e89e-cc71-498e-89ba-9f4c181ab224
 @htl """
 <style>
@@ -35,7 +38,7 @@ figure {margin: 0}
 """
 
 # ╔═╡ f28c18e5-ac1a-4d42-8227-244571d2eadf
-function data_picker_2D(initial_data::Vector{<:Tuple{Float64,Float64,Int64}}=Tuple{Float64,Float64,Int64}[]; xlim=(20,100), ylim=(20,100), radius::Real=4.0, height::Real=300)
+function data_picker_2D(initial_data::Vector{<:Tuple{Float64,Float64,Int64}}=Tuple{Float64,Float64,Int64}[]; xlim=(20,80), ylim=(20,80), radius::Real=4.0, height::Real=300)
     # <script id="yolo">
 	h = @htl """
     <script>
@@ -70,7 +73,10 @@ function data_picker_2D(initial_data::Vector{<:Tuple{Float64,Float64,Int64}}=Tup
 		  ],
 			x: {
 				domain: $(xlim),
-			}
+			},
+		y: {
+				domain: $(ylim),
+			},
 		})
 
 	    div.innerHTML = ""
@@ -93,25 +99,10 @@ render()
 
 
 div.addEventListener("click", (e) => {
-	// if(e.target !== plot) return
-	if(!plot.contains(e.target)) return
-
-console.log(e)
 	const svg = plot.querySelector(":scope > svg") ?? plot
-	const svg_rect = svg.getBoundingClientRect();
-	const rect = svg_rect
-
-		console.log(svg, rect.width, svg.viewBox.baseVal.width)
+	if(!svg.contains(e.target)) return
 		
-	//const p = new DOMPoint(e.clientX, e.clientY).matrixTransform(svg.getScreenCTM().inverse());
-
-		const p = new DOMPoint(
-    (e.clientX - rect.left) / rect.width * svg.viewBox.baseVal.width,
-    (e.clientY - rect.top) / rect.height * svg.viewBox.baseVal.height
-);
-
-		console.log({plot})
-
+	const p = new DOMPoint(e.clientX, e.clientY).matrixTransform(svg.getScreenCTM().inverse());
 
 	const domain_x = plot.scale("x").domain
 	const domain_y = plot.scale("y").domain
@@ -447,6 +438,7 @@ version = "17.4.0+2"
 # ╠═3a311274-1064-11f0-11e1-ab38902d7bb7
 # ╠═0559500b-ec4a-4e75-b1c0-05792203037f
 # ╠═6246d44f-281d-4c19-b58d-f5b9abb6f856
+# ╠═20981265-4bec-49e8-a0a3-1e1179d2f94f
 # ╠═ef95e89e-cc71-498e-89ba-9f4c181ab224
 # ╠═7e206141-bf37-44d3-b9cf-626cc6455a3f
 # ╠═d45bafb5-ed36-49e0-a473-b37c60f1cc78
